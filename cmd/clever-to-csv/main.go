3177ccac091949d90920b5c53b2985127e081936
Alex Zylman <alex.zylman@clever.com>
2014-08-28 10:26:53 -0700

    cmd: one endpoint at a time

--- cmd/clever-to-csv/main.go
+++ cmd/clever-to-csv/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"code.google.com/p/goauth2/oauth"
 	"flag"
-	"fmt"
 	"github.com/Clever/clever-to-csv"
 	csvSink "github.com/azylman/optimus/sinks/csv"
 	"github.com/azylman/optimus/transformer"
@@ -11,49 +10,16 @@ import (
 	"log"
 	"net/url"
 	"os"
-	"sync"
 )
-
-type endpointConf struct {
-	Params url.Values
-	Name   string
-}
-
-func (e *endpointConf) Set(val string) error {
-	if val == "true" {
-		return nil
-	}
-	e.Params = url.Values{"where": []string{val}}
-	return nil
-}
-
-func (e *endpointConf) String() string {
-	return ""
-}
-
-func (e *endpointConf) IsBoolFlag() bool {
-	return true
-}
-
-var acceptedEndpoints = []string{"schools", "sections", "students", "teachers"}
 
 func main() {
 	host := flag.String("host", "https://api.clever.com", "base URL of Clever API")
-	for _, endpoint := range acceptedEndpoints {
-		end := &endpointConf{Name: endpoint}
-		flag.Var(end, endpoint, fmt.Sprintf("if included, dumps the %s endpoint. can optionally include JSON-stringified map of 'where' query parameters", endpoint))
-	}
+	endpoint := flag.String("endpoint", "", "endpoint to download data from")
+	where := flag.String("where", "", "optional where query to limit results")
 	token := flag.String("token", "", "API token to use for authentication")
 	flag.Parse()
 
-	endpoints := []flag.Value{}
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name != "host" && f.Name != "token" {
-			endpoints = append(endpoints, f.Value)
-		}
-	})
-
-	for _, required := range []*string{host, token} {
+	for _, required := range []*string{host, token, endpoint} {
 		if len(*required) == 0 {
 			flag.Usage()
 			os.Exit(1)
@@ -66,32 +32,16 @@ func main() {
 	client := transport.Client()
 	clever := clevergo.New(client, *host)
 
-	wg := sync.WaitGroup{}
-	errors := make(chan error)
-	go func() {
-		wg.Wait()
-		close(errors)
-	}()
-	for _, end := range endpoints {
-		wg.Add(1)
-		go func(endp flag.Value) {
-			defer wg.Done()
-			endpoint, ok := endp.(*endpointConf)
-			if !ok {
-				errors <- fmt.Errorf("invalid flag value %#v", end)
-				return
-			}
-
-			t := transformer.New(clevertable.New(endpoint.Name, endpoint.Params, clever)).
-				Map(clevertable.FlattenRow).
-				Map(clevertable.StringifyArrayVals).
-				Table()
-			if err := csvSink.New(t, endpoint.Name+".csv"); err != nil {
-				errors <- err
-			}
-		}(end)
+	var params url.Values
+	if *where != "" {
+		params = url.Values{"where": []string{*where}}
 	}
-	for err := range errors {
-		log.Fatalf("got err %#v\n", err)
+
+	t := transformer.New(clevertable.New(*endpoint, params, clever)).
+		Map(clevertable.FlattenRow).
+		Map(clevertable.StringifyArrayVals).
+		Table()
+	if err := csvSink.New(t, *endpoint+".csv"); err != nil {
+		log.Fatal(err)
 	}
 }
